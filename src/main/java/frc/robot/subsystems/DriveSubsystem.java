47d9fe1649cc84c3e58bd9a7ae51f63c8b922219
warren yun <xxwarrenhdxx@gmail.com>
2021-11-29 16:55:57 -0500

    drive modes

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,20 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;;
+
 public class DriveSubsystem extends Subsystem {
-    
+
+    public boolean assisted = false;
+    public boolean reversed = false;
+    public double driveMultiplier = 1;
+
+    public void assistedDriveMode() {this.assisted = true;}
+    public void manualDriveMode() {this.assisted = false;}
+
+    @Override
+    protected void initDefaultCommand() {
+		//IGNORE THIS METHOD
+    }
 }
